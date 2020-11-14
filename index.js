a1d7b7b939c3af9178466c11c264b27c144f028b
FabianCMI <fabian.devel@laposte.net>
2020-11-14 18:20:22 +0000

    Merge architecture du jeu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Chargement des modules 
 var express = require('express');
 var app = express();
-var server = app.listen(8080, function() {
+var server = app.listen(8080, function () {
     console.log("C'est parti ! En attente de connexion sur le port 8080...");
 });
 
@@ -11,8 +11,8 @@ var io = require('socket.io').listen(server);
 // Configuration d'express pour utiliser le répertoire "public"
 app.use(express.static('public'));
 // set up to 
-app.get('/', function(req, res) {  
-    res.sendFile(__dirname + '/public/chat.corrige.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/vautour.html');
 });
 
 // déblocage requetes cross-origin
@@ -26,10 +26,13 @@ var clients = {};       // { id -> socket, ... }
 
 
 /***************************************************************
- *              Gestion des défis Chi-Fou-Mi 
+ *           Gestion des parties de stupides vautours
  ***************************************************************/
 
-var chifoumi = require("./rpsls");
+var partieVautour = require("./modules/partie");
+
+// Tableau des parties en cours
+var parties = {};
 
 
 /**
@@ -38,22 +41,22 @@ var chifoumi = require("./rpsls");
  */
 function supprimer(id) {
     delete clients[id];
-    chifoumi.supprimer(id);
+    partieVautour.removePlayer(id);
 }
 
 
 // Quand un client se connecte, on le note dans la console
 io.on('connection', function (socket) {
-    
+
     // message de debug
     console.log("Un client s'est connecté");
     var currentID = null;
-    
+
     /**
      *  Doit être la première action après la connexion.
      *  @param  id  string  l'identifiant saisi par le client
      */
-    socket.on("login", function(id) {
+    socket.on("login", function (id) {
         // si le pseudo est déjà utilisé, on lui envoie l'erreur
         if (clients[id]) {
             socket.emit("erreur-connexion", "Le pseudo est déjà pris.");
@@ -63,11 +66,10 @@ io.on('connection', function (socket) {
         currentID = id;
         // initialisation
         clients[currentID] = socket;
-        chifoumi.ajouter(currentID);
         // log
         console.log("Nouvel utilisateur : " + currentID);
         // scores 
-        var scores = JSON.parse(chifoumi.scoresJSON());
+        var scores = JSON.parse(partieVautour.getScores());
         // envoi d'un message de bienvenue à ce client
         socket.emit("bienvenue", scores);
         // envoi aux autres clients 
@@ -75,14 +77,14 @@ io.on('connection', function (socket) {
         // envoi de la nouvelle liste à tous les clients connectés 
         socket.broadcast.emit("liste", scores);
     });
-    
-    
+
+
     /**
      *  Réception d'un message et transmission à tous.
      *  @param  msg     Object  le message à transférer à tous  
      */
-    socket.on("message", function(msg) {
-        console.log("Reçu message");   
+    socket.on("message", function (msg) {
+        console.log("Reçu message");
         // si message privé, envoi seulement au destinataire
         if (msg.to != null) {
             if (clients[msg.to] !== undefined) {
@@ -102,71 +104,114 @@ io.on('connection', function (socket) {
             io.sockets.emit("message", { from: currentID, to: null, text: msg.text, date: Date.now() });
         }
     });
-    
-    
+
     /**
-     *  Réception d'une demande de défi
+     *  Joue une partie de "stupides Vautours"
      */
-    socket.on("chifoumi", function(data) {
-        data.choice = data.choice.substring(1, data.choice.length - 1);
-        var res = chifoumi.defier(currentID, data.to, data.choice);
-        switch (res.status) {
-            case 1: 
-                clients[data.to].emit("chifoumi", currentID );
-            case -1: 
-            case -2: 
-                socket.emit("message", { from: 0, to: currentID, text: res.message, date: Date.now() });
-                break;
-            case 0:
-                if (res.resultat.vainqueur == null) {
-                    socket.emit("message", { from: 0, to: currentID, text: res.resultat.message, date: Date.now() });
-                    clients[data.to].emit("message", { from: 0, to: data.to, text: res.resultat.message, date: Date.now() });
+    socket.on("jouerVautour", function (gameName) {
+        let currGame = parties[gameName];
+        if (currGame !== undefined) {
+            let curr_card = currGame.getTopPile();
+            while (curr_card) {
+                for (const player of currGame.getPlayersList()) {
+                    if (!player.isAI() && clients[player] === undefined) {
+                        socket.emit("message", { from: null, to: currentID, text: "Joueur " + player + " absent de l'application", date: Date.now() });
+                        return;
+                    }
+                    if (!player.isAI()) {
+                        clients[player].emit("message", {
+                            from: null, to: null,
+                            text: "Début du tour n° " + currGame.getNumTurn(), date: Date.now()
+                        });
+                        // Send the card on top of the pile to the players
+                        clients[player].emit("pile", curr_card);
+                        // Reception of the players' choices
+                        socket.on("choixCarte", data => {
+                            if (data != null) {
+                                if (currGame.getPlayersList()[data.from] === undefined) {
+                                    socket.emit("message", { from: null, to: null, text: "L'utilisateur " + data.from + " n'est pas un joueur de cette partie", date: Date.now() });
+                                    return false;
+                                }
+                                currGame.addCard(player.chooseCard(data.card));
+                            }
+                        });
+                    } else { // AI 
+                        currGame.addCard(player.chooseCard());
+                    }
                 }
-                else {
-                    clients[res.resultat.vainqueur].emit("message", { from: 0, to: res.resultat.vainqueur, text: res.resultat.message + " - c'est gagné", date: Date.now() });
-                    clients[res.resultat.perdant].emit("message", { from: 0, to: res.resultat.perdant, text: res.resultat.message + " - c'est perdu", date: Date.now() });
-                    io.sockets.emit("liste", JSON.parse(chifoumi.scoresJSON()));
-                }
-                break;
+                currGame.playTurn(); // To-do
+                curr_card = currGame.getTopPile();
+            }
+            let winner = currGame.endGame();
+            // TO-DO : End the game and return to menu
+
+        } else {
+            socket.emit("message", { from: null, to: currentID, text: "Aucune partie de ce nom en cours", date: Date.now() });
         }
     });
-    
+    // /**
+    //  *  Réception d'une demande de défi
+    //  */
+    // socket.on("chifoumi", function (data) {
+    //     data.choice = data.choice.substring(1, data.choice.length - 1);
+    //     var res = chifoumi.defier(currentID, data.to, data.choice);
+    //     switch (res.status) {
+    //         case 1:
+    //             clients[data.to].emit("chifoumi", currentID);
+    //         case -1:
+    //         case -2:
+    //             socket.emit("message", { from: 0, to: currentID, text: res.message, date: Date.now() });
+    //             break;
+    //         case 0:
+    //             if (res.resultat.vainqueur == null) {
+    //                 socket.emit("message", { from: 0, to: currentID, text: res.resultat.message, date: Date.now() });
+    //                 clients[data.to].emit("message", { from: 0, to: data.to, text: res.resultat.message, date: Date.now() });
+    //             }
+    //             else {
+    //                 clients[res.resultat.vainqueur].emit("message", { from: 0, to: res.resultat.vainqueur, text: res.resultat.message + " - c'est gagné", date: Date.now() });
+    //                 clients[res.resultat.perdant].emit("message", { from: 0, to: res.resultat.perdant, text: res.resultat.message + " - c'est perdu", date: Date.now() });
+    //                 io.sockets.emit("liste", JSON.parse(vautour.getScores()));
+    //             }
+    //             break;
+    //     }
+    // });
+
     /** 
      *  Gestion des déconnexions
      */
-    
+
     // fermeture
-    socket.on("logout", function() { 
+    socket.on("logout", function () {
         // si client était identifié (devrait toujours être le cas)
         if (currentID) {
             console.log("Sortie de l'utilisateur " + currentID);
             // envoi de l'information de déconnexion
-            socket.broadcast.emit("message", 
-                { from: null, to: null, text: currentID + " a quitté la discussion", date: Date.now() } );
-            // suppression de l'entrée
-            supprimer(currentID);
-            // désinscription du client
-            currentID = null;
-             // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", JSON.parse(chifoumi.scoresJSON()));
-        }
-    });
-    
-    // déconnexion de la socket
-    socket.on("disconnect", function(reason) { 
-        // si client était identifié
-        if (currentID) {
-            // envoi de l'information de déconnexion
-            socket.broadcast.emit("message", 
-                { from: null, to: null, text: currentID + " vient de se déconnecter de l'application", date: Date.now() } );
+            socket.broadcast.emit("message",
+                { from: null, to: null, text: currentID + " a quitté la discussion", date: Date.now() });
             // suppression de l'entrée
             supprimer(currentID);
             // désinscription du client
             currentID = null;
             // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", JSON.parse(chifoumi.scoresJSON()));
+            socket.broadcast.emit("liste", JSON.parse(partieVautour.getScores()));
+        }
+    });
+
+    // déconnexion de la socket
+    socket.on("disconnect", function () {
+        // si client était identifié
+        if (currentID) {
+            // envoi de l'information de déconnexion
+            socket.broadcast.emit("message",
+                { from: null, to: null, text: currentID + " vient de se déconnecter de l'application", date: Date.now() });
+            // suppression de l'entrée
+            supprimer(currentID);
+            // désinscription du client
+            currentID = null;
+            // envoi de la nouvelle liste pour mise à jour
+            socket.broadcast.emit("liste", JSON.parse(partieVautour.getScores()));
         }
         console.log("Client déconnecté");
     });
-        
+
 });
