deda3ec27a730abc6c1d8ff9742ce99c59c059f7
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-16 21:40:28 +0100

    UNE DEMO EN COURS

--- demo.test.js
+++ demo.test.js
@@ -6,20 +6,23 @@ describe('Demo', function () {
 	beforeEach(async function () {
 		// driver = await new Builder().forBrowser('chrome').build(); // For chrome
 
-		driver = await new Builder().forBrowser('firefox').build(); // For firefox
+		driver = await new Builder().forBrowser('chrome').build(); // For chrome
 	});
 
 	afterEach(async function () {
-		// await driver.quit(); // For chrome
-		await driver.close(); // For firefox
+		await driver.quit(); // For chrome
+		//await driver.close(); // For firefox
 	});
 
 	it('ici', async function () {
 		await driver.get('http://localhost:8080/');
 		await driver.manage().window().setRect(1680, 1025);
 
+		await driver.sleep(2000);
+
 		await driver.findElement(By.id('pseudo')).sendKeys('fdadeau');
 		await driver.findElement(By.id('btnConnecter')).click();
+
 		await driver.findElement(By.id('monMessage')).click();
 		await driver.findElement(By.id('monMessage')).sendKeys('bonjour');
 		await driver.findElement(By.id('monMessage')).sendKeys(Key.ENTER);
