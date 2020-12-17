50b913d5cb6c2a04efaf96311a601f67c243bb75
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-17 07:44:06 +0100

    Mouais

--- demo.test.js
+++ demo.test.js
@@ -14,13 +14,16 @@ describe('Demo', function () {
 
 	it('ici', async function () {
 		this.timeout(42000);
+		const windows = [];
+		windows.push(await driver.getWindowHandle());
 
 		// Ouverture du premier onglet
 
 		await driver.get('http://localhost:8080/');
 		await driver.manage().window().setRect(1680, 1025);
 
-		await driver.sleep(2000);
+		switchMode(driver);
+		await driver.sleep(1000);
 
 		connectUser(driver, 'fdadeau');
 		await driver.sleep(1000);
@@ -33,17 +36,27 @@ describe('Demo', function () {
 		openNewTab(driver);
 		connectUser(driver, 'jbernard');
 
-		await driver.sleep(1000);
+		switchTab(driver, windows, 1);
+
+		await driver.sleep(2000);
 	});
 });
 
-const SHORT_BREAK = 1000;
+const SHORT_BREAK = 2000;
 
 /* ---------- Gestion des onglets ---------- */
 
-async function openNewTab(driver) {
+async function openNewTab(driver, tabs) {
 	await driver.switchTo().newWindow('tab');
 	await driver.get('http://localhost:8080/');
+	await driver.getWindowHandle();
+
+	tabs.push(await driver.getWindowHandle());
+}
+
+async function switchTab(driver, tabs, numTab) {
+	await driver.switchTo().window(tabs[numTab - 1]);
+	await driver.sleep(SHORT_BREAK);
 }
 
 /* ---------- Gestion du chat ---------- */
@@ -71,6 +84,11 @@ async function muteVoice(driver) {
 	await driver.sleep(SHORT_BREAK);
 }
 
+async function switchMode(driver) {
+	await driver.actions().keyDown(Key.CONTROL).sendKeys('d').perform();
+	await driver.sleep(SHORT_BREAK);
+}
+
 /* ---------- Gestion de Stupide Vautour ---------- */
 
 async function launchGame(driver) {
@@ -90,18 +108,22 @@ async function answerInvite(driver, accept) {
 	await driver
 		.findElement(By.css(`#popup > button:nth-of-type(${accept ? 1 : 2})`))
 		.click();
+	await driver.sleep(SHORT_BREAK);
 }
 
 async function launchGame(driver) {
 	await driver.findElement(By.id('labelJouerBtn')).click();
+	await driver.sleep(SHORT_BREAK);
 }
 
 async function chooseCard(driver, numCard) {
 	await driver
 		.findElement(By.id(`#main-joueur > .carte-joueur:nth-of-type(${numCard})`))
 		.click();
+	await driver.sleep(SHORT_BREAK);
 }
 
 async function removePopup(driver) {
 	await driver.findElement(By.id('popup')).click();
+	await driver.sleep(SHORT_BREAK);
 }
