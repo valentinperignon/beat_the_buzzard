de4171f0061f117306e942fc47f1e9ff5f5368d6
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-17 07:32:00 +0100

    fonctions qui marchent bof

--- demo.test.js
+++ demo.test.js
@@ -15,8 +15,6 @@ describe('Demo', function () {
 	it('ici', async function () {
 		this.timeout(42000);
 
-		let tabs = null;
-
 		// Ouverture du premier onglet
 
 		await driver.get('http://localhost:8080/');
@@ -24,18 +22,86 @@ describe('Demo', function () {
 
 		await driver.sleep(2000);
 
-		await driver.findElement(By.id('pseudo')).sendKeys('dadeau');
+		connectUser(driver, 'fdadeau');
 		await driver.sleep(1000);
-		await driver.findElement(By.id('btnConnecter')).click();
+		sendMessageChat(driver, 'Salut à tous.');
 
 		await driver.sleep(2000);
 
 		// Ouverture du deuxième onglet
 
-		await driver.switchTo().newWindow('tab');
-		await driver.get('http://localhost:8080/');
-	
-  });
-  
-  function connectUser()
+		openNewTab(driver);
+		connectUser(driver, 'jbernard');
+
+		await driver.sleep(1000);
+	});
 });
+
+const SHORT_BREAK = 1000;
+
+/* ---------- Gestion des onglets ---------- */
+
+async function openNewTab(driver) {
+	await driver.switchTo().newWindow('tab');
+	await driver.get('http://localhost:8080/');
+}
+
+/* ---------- Gestion du chat ---------- */
+
+async function connectUser(driver, username) {
+	await driver.findElement(By.id('pseudo')).sendKeys(username);
+	await driver.sleep(SHORT_BREAK);
+	await driver.findElement(By.id('btnConnecter')).click();
+	await driver.sleep(SHORT_BREAK);
+}
+
+async function sendMessageChat(driver, message) {
+	await driver.findElement(By.id('monMessage')).sendKeys(message);
+	await driver.sleep(SHORT_BREAK);
+	await driver.findElement(By.id('monMessage')).sendKeys(Key.ENTER);
+	await driver.sleep(SHORT_BREAK);
+}
+
+async function muteVoice(driver) {
+	await driver.findElement(By.id('syntheseBtn')).click();
+	await driver.sleep(SHORT_BREAK);
+	await driver.findElement(By.id('mute')).click();
+	await driver.sleep(SHORT_BREAK);
+	await driver.findElement(By.css('.afficherFenetre > button')).click();
+	await driver.sleep(SHORT_BREAK);
+}
+
+/* ---------- Gestion de Stupide Vautour ---------- */
+
+async function launchGame(driver) {
+	sendMessageChat(driver, '/vautour');
+}
+
+async function invitePlayer(driver, playerNum) {
+	await driver.findElement(By.id('carte-ajout-joueur')).click();
+	await driver.sleep(SHORT_BREAK);
+	await driver
+		.findElement(By.css(`#choix-utilisateurs > li:nth-child(${playerNum})`))
+		.click();
+	await driver.sleep(SHORT_BREAK);
+}
+
+async function answerInvite(driver, accept) {
+	await driver
+		.findElement(By.css(`#popup > button:nth-of-type(${accept ? 1 : 2})`))
+		.click();
+}
+
+async function launchGame(driver) {
+	await driver.findElement(By.id('labelJouerBtn')).click();
+}
+
+async function chooseCard(driver, numCard) {
+	await driver
+		.findElement(By.id(`#main-joueur > .carte-joueur:nth-of-type(${numCard})`))
+		.click();
+}
+
+async function removePopup(driver) {
+	await driver.findElement(By.id('popup')).click();
+}
